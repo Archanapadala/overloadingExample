e81278faab7ed6807c43a60e2143d2b677b62787
archa <archa@DESKTOP-T17NCFH>
2024-05-04 09:35:32 -0500

    Add project files.

---
+++ overloadingExample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace overloadingExample
+{
+    public class MathOperat
+    {
+        public int Add(int x , int y)
+        {
+            return x + y;
+        }
+
+        public double Add(double x , double y)
+        {
+            return x + y;
+        }
+         public string Add(string x , string y)
+        {
+            return x + y;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                MathOperat math = new MathOperat();
+
+                Console.WriteLine("Adding two integers:" + math.Add(5, 3));
+                Console.WriteLine("Adding two doubles:" + math.Add(5.5, 3.3));
+                Console.WriteLine("two strings :" + math.Add("Hello", "team"));
+            }
+        }
+
+    }
+}
